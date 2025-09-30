fea18782ff24998fdfeea90a519eeca4b1844f00
mahadi-init <init.mahadi@gmail.com>
2025-10-01 04:36:05 +0600

    bring back fidget nvim

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -39,7 +39,7 @@ return {
 				lualine_a = { "mode" },
 				lualine_b = { "branch", "diff", "diagnostics" },
 				lualine_c = { { "filename", path = 1 } },
-				lualine_x = { "lsp_status", "encoding", "fileformat", "filetype" },
+				lualine_x = { "encoding", "fileformat", "filetype" },
 				lualine_y = { "progress" },
 				lualine_z = { "location" },
 			},
