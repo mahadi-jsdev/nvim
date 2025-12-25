6dd70d48e7b03ae6787875d96730a2b2d8c4fb4f
polyculture <ryan@polyculturegroup.com>
2025-12-26 00:04:49 +0600

    modify some plugins

---
+++ lua/plugins/arrow.lua
@@ -0,0 +1,12 @@
+return {
+	"otavioschwanck/arrow.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	opts = {
+		show_icons = true,
+		leader_key = ";", -- Recommended to be a single key
+		buffer_leader_key = "m", -- Per Buffer Mappings
+	},
+}
