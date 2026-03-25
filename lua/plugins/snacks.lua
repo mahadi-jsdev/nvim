00e5828986049062a473c0ee55047107a0de248c
mahadi-jsdev <mahadi.jsdev@gmail.com>
2026-03-25 12:50:35 +0600

    feat(ui): add changes to plugins

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,38 +1,31 @@
 -- Snacks for features that telescope doesn't handle
 return {
-	"folke/snacks.nvim",
-	lazy = false,
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	opts = {
-		bigfile = {},
-		quickfile = {},
-		bufDelete = {},
-		statuscolumn = {},
-		scope = {},
-		input = {},
-		notifier = {},
-		picker = {},
-		indent = {},
-		explorer = {},
-		dashboard = {},
-		-- image = {},
-	},
-	keys = {
-		{
-			"<leader>e",
-			function()
-				Snacks.explorer()
-			end,
-			desc = "explorer",
-		},
-		{
-			"<C-x>",
-			function()
-				Snacks.bufdelete()
-			end,
-			desc = "Delete Buffer",
-		},
-	},
+  "folke/snacks.nvim",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    bigfile = {},
+    quickfile = {},
+    bufDelete = {},
+    statuscolumn = {},
+    scope = {},
+    input = {},
+    notifier = {},
+    picker = {},
+    indent = {},
+    explorer = {},
+    dashboard = {},
+    -- image = {},
+  },
+  keys = {
+    {
+      "<leader>e",
+      function()
+        Snacks.explorer()
+      end,
+      desc = "explorer",
+    },
+  },
 }
---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,6 @@
+return {
+  "folke/todo-comments.nvim",
+  event = "BufRead",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {}
+}
