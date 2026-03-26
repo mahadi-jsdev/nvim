b52715d1c1a57f261533d534247a04c454ef8be0
mahadi-jsdev <mahadi.jsdev@gmail.com>
2026-03-26 21:20:17 +0600

    refactor: fixed blink cmp issue

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -46,6 +46,12 @@ return {
             },
           },
         },
+        list = {
+          selection = {
+            preselect = false,
+            auto_insert = false,
+          },
+        },
         documentation = { auto_show = true, auto_show_delay_ms = 500 },
       },
     })
