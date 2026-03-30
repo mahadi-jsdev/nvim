6ffd4c676e40ecfdfc1bd80d24f405a8c853f12b
mahadi-jsdev <mahadi.jsdev@gmail.com>
2026-03-30 16:32:22 +0600

    update blink config

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -21,7 +21,7 @@ return {
       sources = {
         default = {
           "lsp",
-          "buffer",
+          -- "buffer",
           "path",
           "snippets",
         },
@@ -46,13 +46,7 @@ return {
             },
           },
         },
-        list = {
-          selection = {
-            preselect = false,
-            auto_insert = false,
-          },
-        },
-        documentation = { auto_show = true, auto_show_delay_ms = 500 },
+        documentation = { auto_show = true, auto_show_delay_ms = 300 },
       },
     })
   end,
