06cde4a89f4ecc132354ed8ae35079c61a614564
mahadi-jsdev <mahadi.jsdev@gmail.com>
2026-04-01 21:18:54 +0600

    change some plugin configs

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -22,64 +22,11 @@ return {
   },
   keys = {
     {
-      "<leader><leader>",
-      function()
-        Snacks.picker.files()
-      end,
-      desc = "search files",
-    },
-    {
-      ",",
-      function()
-        Snacks.picker.buffers()
-      end,
-      mode = { "n" },
-      desc = "buffers",
-    },
-    {
       "<leader>e",
       function()
         Snacks.explorer()
       end,
       desc = "explorer",
-    },
-    {
-      "<C-f>",
-      function()
-        Snacks.picker.grep()
-      end,
-      desc = "Grep",
-      mode = { "n" },
-    },
-    {
-      "<C-f>",
-      function()
-        Snacks.picker.grep_word()
-      end,
-      desc = "Visual selection or word",
-      mode = { "x" },
-    },
-    {
-      "<leader>fl",
-      function()
-        Snacks.picker.lines()
-      end,
-      desc = "find lines",
-      mode = { "n" },
-    },
-    {
-      "<C-g>",
-      function()
-        Snacks.picker.git_status()
-      end,
-      desc = "Git Status",
-    },
-    {
-      "<C-x>",
-      function()
-        Snacks.bufdelete()
-      end,
-      desc = "Delete Buffer",
     },
     {
       "<C-t>",
