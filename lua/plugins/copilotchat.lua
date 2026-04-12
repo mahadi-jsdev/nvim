bb11f5060cc9f43ef1aa4b340cd8d84a50f58fa8
mahadi-jsdev <mahadi.jsdev@gmail.com>
2026-04-12 15:22:05 +0600

    feat: add CopilotChat.nvim plugin with commit prompt
    - Added CopilotChat.nvim plugin configuration to lua/plugins/copilotchat.lua
    - Configured dependency on plenary.nvim and set build step for tiktoken
    - Defined a custom "Commit" prompt for generating commit messages using commitizen convention
    - Mapped <leader>cm to trigger CopilotChatCommit for easy access
    - Updated lazy-lock.json to include CopilotChat.nvim with its commit hash

---
+++ lua/plugins/copilotchat.lua
@@ -0,0 +1,25 @@
+return {
+  "CopilotC-Nvim/CopilotChat.nvim",
+  dependencies = {
+    { "nvim-lua/plenary.nvim", branch = "master" },
+  },
+  build = "make tiktoken",
+  opts = {
+    prompts = {
+      Commit = {
+        prompt =
+        [[ Write commit message for the change with commitizen convention. Give a good title under 50 characters and make commit message as list and as detailed as possible. Format as a gitcommit code block. ]],
+        resources = {
+          "gitdiff:staged",
+        },
+      },
+    },
+  },
+  keys = {
+    {
+      "<leader>cm",
+      "<CMD>CopilotChatCommit<CR>",
+      desc = "Copilot Chat commit",
+    },
+  },
+}
