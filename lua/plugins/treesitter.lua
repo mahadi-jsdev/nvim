d362c345a4210b4b94eab501c945089866869b2c
mahadi-jsdev <mahadi.jsdev@gmail.com>
2026-04-13 12:23:10 +0600

    fix treesitter issue

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,6 +4,21 @@ return {
   lazy = false,
   build = ":TSUpdate",
   config = function()
+    local ts = vim.treesitter
+    local original_get_node_text = ts.get_node_text
+
+    ts.get_node_text = function(node, source, opts)
+      if type(node) == "table" then
+        node = node[1]
+      end
+
+      if not node then
+        return ""
+      end
+
+      return original_get_node_text(node, source, opts)
+    end
+
     require("nvim-treesitter.configs").setup({
       ensure_installed = {
         "lua",
