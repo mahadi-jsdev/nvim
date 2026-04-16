b6bcd8722ced57db71967bfe849854c896306dc5
mahadi-jsdev <mahadi.jsdev@gmail.com>
2026-04-16 15:56:29 +0600

    improve harpoon

--- lua/plugins/harpoon-core.lua
+++ lua/plugins/harpoon-core.lua
@@ -9,20 +9,62 @@ function M.setup()
   harpoon.setup({})
 
   local map = vim.keymap.set
+  local function info(message)
+    print(message)
+  end
+
+  local function current_name()
+    local name = vim.fn.expand("%:t")
+    if name == "" then
+      return "[No Name]"
+    end
+
+    return name
+  end
 
   -- harpoon core
-  map("n", "<leader>ml", ":lua require('harpoon-core').toggle_quick_menu()<CR>", { desc = "harpoon view" })
-  map("n", "<leader>ma", ":lua require('harpoon-core').add_file()<CR>", { desc = "harpoon add" })
-  map("n", "<leader>mr", ":lua require('harpoon-core').rm_file()<CR>", { desc = "harpoon add" })
-  map("n", "<C-l>", ":lua require('harpoon-core').nav_next()<CR>", { desc = "harpoon next" })
-  map("n", "<C-h>", ":lua require('harpoon-core').nav_prev()<CR>", { desc = "harpoon prev" })
+  map("n", "<leader>ml", function()
+    harpoon.toggle_quick_menu()
+    info("Harpoon menu toggled")
+  end, { desc = "harpoon view" })
+  map("n", "<leader>ma", function()
+    harpoon.add_file()
+    info("File added: " .. current_name())
+  end, { desc = "harpoon add" })
+  map("n", "<leader>mr", function()
+    harpoon.rm_file()
+    info("File removed: " .. current_name())
+  end, { desc = "harpoon add" })
+  map("n", "<C-l>", function()
+    harpoon.nav_next()
+    info("Moved to next harpoon file")
+  end, { desc = "harpoon next" })
+  map("n", "<C-h>", function()
+    harpoon.nav_prev()
+    info("Moved to previous harpoon file")
+  end, { desc = "harpoon prev" })
 
   -- number based navigation
-  map("n", "<A-1>", ":lua require('harpoon-core').nav_file(1)<CR>", { desc = "harpoon 1" })
-  map("n", "<A-2>", ":lua require('harpoon-core').nav_file(2)<CR>", { desc = "harpoon 2" })
-  map("n", "<A-3>", ":lua require('harpoon-core').nav_file(3)<CR>", { desc = "harpoon 3" })
-  map("n", "<A-4>", ":lua require('harpoon-core').nav_file(4)<CR>", { desc = "harpoon 4" })
-  map("n", "<A-5>", ":lua require('harpoon-core').nav_file(5)<CR>", { desc = "harpoon 5" })
+  map("n", "<A-1>", function()
+    harpoon.nav_file(1)
+    info("Moved to harpoon file 1")
+  end, { desc = "harpoon 1" })
+  map("n", "<A-2>", function()
+    harpoon.nav_file(2)
+    info("Moved to harpoon file 2")
+  end, { desc = "harpoon 2" })
+  map("n", "<A-3>", function()
+    harpoon.nav_file(3)
+    info("Moved to harpoon file 3")
+  end, { desc = "harpoon 3" })
+  map("n", "<A-4>", function()
+    harpoon.nav_file(4)
+    info("Moved to harpoon file 4")
+  end, { desc = "harpoon 4" })
+  map("n", "<A-5>", function()
+    harpoon.nav_file(5)
+    info("Moved to harpoon file 5")
+  end, { desc = "harpoon 5" })
 end
 
 return M
