c7ca193dd25002b877a1a5684aedd8ad8bab26fd
mahadi-jsdev <mahadi.jsdev@gmail.com>
2026-04-19 21:25:22 +0600

    setup kulala plugin

--- init.lua
+++ init.lua
@@ -72,6 +72,18 @@ require("lazy").setup({
     end,
   },
   {
+    "mistweaverco/kulala.nvim",
+    keys = {
+      { "<leader>rs", desc = "Send request" },
+      { "<leader>ra", desc = "Send all requests" },
+      { "<leader>rb", desc = "Open scratchpad" },
+    },
+    ft = { "http", "rest" },
+    config = function()
+      require("plugins.kulala").setup()
+    end,
+  },
+  {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPre", "BufNewFile" },
     config = function()
