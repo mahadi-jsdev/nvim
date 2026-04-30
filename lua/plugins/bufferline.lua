431ab4fc480b99654f8df9e6839028f929f41a11
mahadi-jsdev <mahadi.jsdev@gmail.com>
2026-04-30 11:58:44 +0600

    changes

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -14,25 +14,13 @@ function M.setup()
   map("n", "<C-h>", "<CMD>BufferLineCyclePrev<CR>", { desc = "Previous buffer" })
   map("n", "<C-Right>", "<CMD>BufferLineMoveNext<CR>", { desc = "Move Next buffer" })
   map("n", "<C-Left>", "<CMD>BufferLineMovePrev<CR>", { desc = "Previous buffer" })
-  map("n", "<C-p>", "<CMD>BufferLinePick<CR>", { desc = "BufferPick" })
 
   -- actions
-  map("n", "<leader>bsr", "<CMD>BufferLineSortByRelativeDirectory<CR>", { desc = "Relative" })
+  map("n", "<leader>bs", "<CMD>BufferLineSortByRelativeDirectory<CR>", { desc = "Relative" })
   map("n", "<leader>bp", "<CMD>BufferLineTogglePin<CR>")
   map("n", "<leader>bcr", "<CMD>BufferLineCloseRight<CR>")
   map("n", "<leader>bcl", "<CMD>BufferLineCloseLeft<CR>")
   map("n", "<leader>bco", "<CMD>BufferLineCloseOthers<CR>")
-
-  -- go to
-  map("n", "<leader>1", "<CMD>BufferLineGoToBuffer 1<CR>", { desc = "go to" })
-  map("n", "<leader>2", "<CMD>BufferLineGoToBuffer 2<CR>", { desc = "go to" })
-  map("n", "<leader>3", "<CMD>BufferLineGoToBuffer 3<CR>", { desc = "go to" })
-  map("n", "<leader>4", "<CMD>BufferLineGoToBuffer 4<CR>", { desc = "go to" })
-  map("n", "<leader>5", "<CMD>BufferLineGoToBuffer 5<CR>", { desc = "go to" })
-  map("n", "<leader>6", "<CMD>BufferLineGoToBuffer 6<CR>", { desc = "go to" })
-  map("n", "<leader>7", "<CMD>BufferLineGoToBuffer 7<CR>", { desc = "go to" })
-  map("n", "<leader>8", "<CMD>BufferLineGoToBuffer 8<CR>", { desc = "go to" })
-  map("n", "<leader>9", "<CMD>BufferLineGoToBuffer 9<CR>", { desc = "go to" })
 end
 
 return M
